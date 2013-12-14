0c6f4a56ffc3b071940a253e72b8e2728b50845d
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2013-12-14 17:06:31 +0400

    testing heroku added configuration files

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ var express = require('express'),
 
 app.use(app.router);
 app.use(express.static(pub_dir));
-app.use(express.errorHandler());
 
 app.set('views', __dirname + '/views');
 app.set('view engene', 'jade');
+
+if (app.get('env') === 'development') {
+  console.log('development');
+} else {
+  console.log(app.get('env'), ' prod bitches');
+}
 
 app.listen(port, function () {
   console.log('listening on port:', port);
