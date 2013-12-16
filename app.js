4083be2fa04c37ef3907b8f125f2b6e556d3aa32
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2013-12-16 22:02:23 +0400

    added less middleware

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express'),
     pub_dir = __dirname + '/public',
     config = require('nconf').argv().env();
 
+global.Middlewares = require(__dirname + '/middlewares');
+
 // enviroment specific configuration
 if (app.get('env') === 'development') {
   config.file('local', __dirname + '/configs/local.json');
+  app.use(Middlewares.less());
 }
 config.file('config', __dirname + '/configs/config.json');
 
@@ -13,8 +16,6 @@ config.file('config', __dirname + '/configs/config.json');
 global.config = config;
 global.mongoose = require('mongoose');
 global.base_dir = __dirname;
-
-global.Middlewares = require(__dirname + '/middlewares');
 global.Models = require(__dirname + '/models');
 
 app.use(Middlewares.base_dir);
--- middlewares/index.js
+++ middlewares/index.js
@@ -6,3 +6,15 @@ exports.base_dir = function (req, res, next) {
   res.locals.basedir = __dirname + '/../views';
   next()
 };
+
+exports.less = function () {
+  var less = require("less-middleware");
+  
+  return less({
+    dest: __dirname + "/../public/css",
+    src: __dirname + "/../public/less",
+    prefix: "/css",
+    compress: true,
+    force: true,
+  });
+}
